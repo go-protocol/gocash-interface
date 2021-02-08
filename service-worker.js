58507a42d4dfe6a85a6e3f20c23e80b763bfbf5b
fankouzu <1046166@qq.com>
2021-02-08 20:56:08 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18488eaa9cd6052e052e6c942edc1bee.js"
+  "/precache-manifest.caad161306cecc952e0af845533e2394.js"
 );
 
 self.addEventListener('message', (event) => {
