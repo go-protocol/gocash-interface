2b8678172009bb3157ae1df7e6410af98f9a9f77
marui <1016595756@qq.com>
2021-02-23 22:41:07 +0800

    添加治理按钮

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -9,7 +9,7 @@ const Nav: React.FC = () => {
       <StyledLink exact activeClassName="active" to="/bank">赛道</StyledLink>
       <StyledLink exact activeClassName="active" to="/bonds">债券</StyledLink>
       <StyledLink exact activeClassName="active" to="/boardroom">董事会</StyledLink>
-      {/* <StyledLink2 href="https://snapshot.page/#/basiscash.eth" target="_blank" >Vote</StyledLink2> */}
+      <StyledLink2 href="https://heco.vote/" target="_blank">治理</StyledLink2>
     </StyledNav>
   )
 }
@@ -35,18 +35,15 @@ const StyledLink = styled(NavLink)`
     color: ${props => props.theme.color.primary.main};
   }
 `
-// const StyledLink2 = styled.a`
-//   color: ${props => props.theme.color.grey[400]};
-//   font-weight: 700;
-//   padding-left: ${props => props.theme.spacing[3]}px;
-//   padding-right: ${props => props.theme.spacing[3]}px;
-//   text-decoration: none;
-//   &:hover {
-//     color: ${props => props.theme.color.grey[500]};
-//   }
-//   &.active {
-//     color: ${props => props.theme.color.primary.main};
-//   }
-// `;// eslint-disable-line no-unused-vars
+const StyledLink2 = styled.a`
+  color: ${props => props.theme.color.grey[400]};
+  font-weight: 700;
+  padding-left: ${props => props.theme.spacing[3]}px;
+  padding-right: ${props => props.theme.spacing[3]}px;
+  text-decoration: none;
+  &:hover {
+    color: ${props => props.theme.color.grey[500]};
+  }
+`
 
 export default Nav
